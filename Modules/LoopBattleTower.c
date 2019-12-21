64659e96fe98077e948d911e288ff7f6c1cda4ea
tsalvia <tsalvia.ctf@gmail.com>
2019-12-22 03:32:40 +0900

    Fix indent

--- Modules/LoopBattleTower.c
+++ Modules/LoopBattleTower.c
@@ -15,61 +15,61 @@ static uint16_t duration_count = 0;
 
 void LoopBattleTower_Module(USB_JoystickReport_Input_t* const ReportData)
 {
-    switch (duration_count) {
-    case 0 ... 199:
-        /* AA */
-	    if (duration_count % 100 < 25)
-		    ReportData->Button |= SWITCH_A;
-        break;
-    case 200 ... 2299:
-        /* wait 17sec */
-        break;
-    case 2300 ... 2499:
-        /* AA */
-	    if (duration_count % 100 < 25)
-		    ReportData->Button |= SWITCH_A;
-        break;
-    case 2500 ... 2599:
-        /* UP */
+	switch (duration_count) {
+	case 0 ... 199:
+		/* AA */
+		if (duration_count % 100 < 25)
+			ReportData->Button |= SWITCH_A;
+		break;
+	case 200 ... 2299:
+		/* wait 17sec */
+		break;
+	case 2300 ... 2499:
+		/* AA */
+		if (duration_count % 100 < 25)
+			ReportData->Button |= SWITCH_A;
+		break;
+	case 2500 ... 2599:
+		/* UP */
 		if (duration_count % 100 < 25)
 			ReportData->HAT = HAT_TOP;
-        break;
-    case 2600 ... 2799:
-        /* AA */
-	    if (duration_count % 100 < 25)
-		    ReportData->Button |= SWITCH_A;
-        break;
-    case 2800 ... 2899:
-        /* UP */
+		break;
+	case 2600 ... 2799:
+		/* AA */
+		if (duration_count % 100 < 25)
+			ReportData->Button |= SWITCH_A;
+		break;
+	case 2800 ... 2899:
+		/* UP */
 		if (duration_count % 100 < 25)
 			ReportData->HAT = HAT_TOP;
-        break;
-    case 2900 ... 3199:
-        /* AAA */
+		break;
+	case 2900 ... 3199:
+		/* AAA */
 		if (duration_count % 100 < 25)
-		    ReportData->Button |= SWITCH_A;
-        break;
-    case 3200 ... 5299:
-        /* wait 17sec */
-        break;
-    case 5300 ... 5499:
-        /* AA */
+			ReportData->Button |= SWITCH_A;
+		break;
+	case 3200 ... 5299:
+		/* wait 17sec */
+		break;
+	case 5300 ... 5499:
+		/* AA */
 		if (duration_count % 100 < 25)
-		    ReportData->Button |= SWITCH_A;
-        break;
-    case 5500 ... 5699:
-        /* BB */
+			ReportData->Button |= SWITCH_A;
+		break;
+	case 5500 ... 5699:
+		/* BB */
 		if (duration_count % 100 < 25)
-		    ReportData->Button |= SWITCH_B;
-        break;
-    case 5700 ... 5799:
-        /* UP */
+			ReportData->Button |= SWITCH_B;
+		break;
+	case 5700 ... 5799:
+		/* UP */
 		if (duration_count % 100 < 25)
 			ReportData->HAT = HAT_TOP;
-        break;
-    case 5800:
-        duration_count = 0;
-        return;
-    }
+		break;
+	case 5800:
+		duration_count = 0;
+		return;
+	}
 	duration_count++;
 }
