9a687d56d8e00def77b9bc9478b1fc4ad13dfa03
Almtr <almtr-0906@hotmail.co.jp>
2020-01-02 02:07:20 +0900

    Removed unnecessary external functions

--- Modules/Modules.h
+++ Modules/Modules.h
@@ -1,7 +1,6 @@
 #ifndef _MODULES_H_
 #define _MODULES_H_
 
-extern uint8_t SyncController(USB_JoystickReport_Input_t* const ReportData, uint16_t count);
 extern void HatchEggs_Module(USB_JoystickReport_Input_t* const ReportData);
 extern void LoopTournament_Module(USB_JoystickReport_Input_t* const ReportData);
 extern void RepeatA_Module(USB_JoystickReport_Input_t* const ReportData);
